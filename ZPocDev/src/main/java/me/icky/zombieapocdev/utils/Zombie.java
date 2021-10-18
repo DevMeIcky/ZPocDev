893df326bf36966fe36a4626111786838b18e357
DevMeIcky <92671792+DevMeIcky@users.noreply.github.com>
2021-10-18 12:17:27 +0500

    Update

--- ZPocDev/src/main/java/me/icky/zombieapocdev/utils/Zombie.java
+++ ZPocDev/src/main/java/me/icky/zombieapocdev/utils/Zombie.java
@@ -26,4 +26,21 @@ public class Zombie {
         zombie.setCanPickupItems(pickItems);
         player.sendMessage(Main.prefix + "§rSpawned zombie.");
     }
+
+    public static void spawnZombie(String name, String health, String strength, String speed,
+                                   String jump, String range, String pickItems, World world, Location location, Player player) {
+
+
+        org.bukkit.entity.Zombie zombie = (org.bukkit.entity.Zombie) world.spawnEntity(location, EntityType.ZOMBIE);
+        zombie.setCustomNameVisible(true);
+        zombie.setCustomName(name);
+        zombie.setMaxHealth(Integer.valueOf(health));
+        zombie.setHealth(Integer.valueOf(health));
+        zombie.getAttribute(Attribute.GENERIC_ATTACK_DAMAGE).setBaseValue(Integer.valueOf(health));
+        zombie.getAttribute(Attribute.GENERIC_MOVEMENT_SPEED).setBaseValue(Integer.valueOf(strength));
+        zombie.addPotionEffect(new PotionEffect(PotionEffectType.JUMP, 18400000, Integer.valueOf(jump)));
+        zombie.getAttribute(Attribute.GENERIC_FOLLOW_RANGE).setBaseValue(Integer.valueOf(range));
+        zombie.setCanPickupItems(Boolean.valueOf(pickItems));
+        player.sendMessage(Main.prefix + "§rSpawned zombie.");
+    }
 }
